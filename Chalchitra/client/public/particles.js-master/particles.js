2b2dd1583577969f47fdd9db4cadae2f49af6052
Aaryan Saroha <aaryansaroha12@gmail.com>
2026-03-10 16:42:23 +0530

    Remove Chalchitra subtree

--- Chalchitra/client/public/particles.js-master/particles.js
+++
@@ -1,5 +0,0 @@
-/* Particles.js - Empty placeholder for deployment */
-/* If you need the full particles.js library, install via npm: */
-/* npm install particles.js */
-window.particlesJS = window.particlesJS || {};
-
