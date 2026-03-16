2f04d8b6d359f3054a2de1334e03dde59710caab
Aaryan Saroha <aaryansaroha12@gmail.com>
2026-03-17 01:38:29 +0530

    Add GA4 gtag bootstrap and set measurement ID

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// --- Google Analytics 4 (gtag.js) bootstrap ---
+const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
+
+if (GA_MEASUREMENT_ID) {
+  // Load the gtag script
+  const script = document.createElement('script');
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  script.async = true;
+  document.head.appendChild(script);
+
+  // Initialize dataLayer and configure GA
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', GA_MEASUREMENT_ID);
+} else {
+  // eslint-disable-next-line no-console
+  console.warn('GA measurement ID not set (REACT_APP_GA_MEASUREMENT_ID). Analytics disabled.');
+}
+// --- end GA bootstrap ---
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
