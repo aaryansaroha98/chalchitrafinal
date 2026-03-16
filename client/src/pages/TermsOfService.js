87f0853e25eaa0da2d929db4a1cdb746a17ad04e
Aaryan Saroha <aaryansaroha12@gmail.com>
2026-03-16 16:54:54 +0530

    Remove last updated note from terms

--- client/src/pages/TermsOfService.js
+++ client/src/pages/TermsOfService.js
@@ -35,13 +35,6 @@ const TermsOfService = () => {
               }}>
                 Terms of Service
               </h1>
-              <p className="policy-subtitle terms-subtitle" style={{
-                color: '#cccccc',
-                fontSize: '1rem',
-                margin: 0
-              }}>
-                Last updated: {new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
-              </p>
             </div>
 
             {/* Content */}
